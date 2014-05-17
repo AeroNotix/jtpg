72d3f0a78d6206f6b904837d5a05452aa96c8f31
Aaron France <aaron.l.france@gmail.com>
2014-05-18 00:49:01 +0200

    Basic skeleton of a Jepsen test.

--- jtpg/test/jtpg/pg2_test.clj
+++ jtpg/test/jtpg/pg2_test.clj
@@ -1,3 +1,39 @@
 (ns jtpg.pg2-test
-  (:require [jtpg.pg2 :refer :all]
-            [clojure.test :refer :all]))
+  (:use     [jepsen.core])
+  (:require [clojure.test     :refer :all]
+            [jepsen.checker   :as checker]
+            [jepsen.db        :as jdb]
+            [jepsen.generator :as gen]
+            [jepsen.model     :as model]
+            [jepsen.nemesis   :as nemesis]
+            [jepsen.os        :as os]
+            [jepsen.client    :as client]
+            [jepsen.os.debian :as debian]
+            [jepsen.report    :as report]
+            [jepsen.store     :as store]
+            [jtpg.pg2         :refer :all]))
+
+
+(def noop-test
+  "Boring test stub"
+  {:nodes     [:n1 :n2 :n3 :n4 :n5]
+   :os        os/noop
+   :db        jdb/noop
+   :client    client/noop
+   :nemesis   client/noop
+   :generator gen/void
+   :model     model/noop
+   :checker   checker/linearizable})
+
+(deftest group-test
+  (binding [jepsen.control/*username* "root"
+            jepsen.control/*password* "root"
+            jepsen.control/*strict-host-key-checking* :no]
+    (let [test (run!
+                 (assoc noop-test
+                   :name "pg2"
+                   :os debian/os
+                   :db db
+                   :client (create-node-list-client)
+                   :nemesis (nemesis/partitioner nemesis/bridge)))]
+      (println test))))
