498e25715496bf6e55eb566b0946757f49aca98a
Aaron France <aaron.l.france@gmail.com>
2014-05-18 00:47:18 +0200

    Add dependencies.

--- jtpg/project.clj
+++ jtpg/project.clj
@@ -4,6 +4,9 @@
   :url "http://github.com/AeroNotix/jtpg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [jepsen "0.0.3-SNAPSHOT"]
+                 [cheshire "5.3.1"]
+                 [http-kit "2.1.18"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
