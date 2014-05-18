6d8bd30516d539ebd91e1953ff85e848b461e9b8
Aaron France <aaron.l.france@gmail.com>
2014-05-18 02:23:54 +0200

    Add initial test client.

--- jtpg/src/jtpg/pg2.clj
+++ jtpg/src/jtpg/pg2.clj
@@ -1,23 +1,27 @@
 (ns jtpg.pg2
   (:require [jepsen.db :as db]
+            [jepsen.control.util :as cutil]
+            [cheshire.core :as json]
             [jepsen.control :as c]
+            [jepsen.client :as client]
             [org.httpkit.client :as http]))
 
 
 (def repo-name "jtpg")
 (def git-repo (format "https://github.com/AeroNotix/%s.git" repo-name))
+(def git-dir (str "/tmp/" repo-name))
 
 (def db
   (reify db/DB
     (setup! [_ test node]
-      (c/su
-        (c/cd "/tmp"
-          (c/exec :git :clone git-repo))
-        (c/cd (str "/tmp/" git-repo)
-          (c/exec :make))))
-    (teardown! [_ test node]
-      (c/su
-        (c/exec :rm :-rf (str "/tmp/" repo-name))))))
+      (when (not (cutil/file? git-dir))
+        (c/su
+          (c/cd "/tmp"
+            (c/exec :git :clone git-repo))))
+      (c/cd (str "/tmp/" repo-name "/erl")
+        (c/exec :make)))
+    (teardown! [_ _ _])))
+
 (defn create-new-pid [client n]
   (let [uri (str (.endpoint client) n)
         resp @(http/get uri)]
@@ -30,6 +34,9 @@
       (Pg2NodeListClient. endpoint)))
 
   (invoke! [_ test op]
+    (throw (UnsupportedOperationException.)))
+
+  (teardown! [_ _]
     (throw (UnsupportedOperationException.))))
 
 (defn create-node-list-client []
