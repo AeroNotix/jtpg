c51445128aa3f73350270d2c743d282d85986b64
Aaron France <aaron.l.france@gmail.com>
2014-05-18 15:39:52 +0200

    More logging.

--- jtpg/src/jtpg/pg2.clj
+++ jtpg/src/jtpg/pg2.clj
@@ -21,10 +21,13 @@
           (c/cd "/tmp"
             (c/exec :git :clone git-repo))))
       (c/cd (str "/tmp/" repo-name "/erl")
+        (info "Location of release: " (str "/tmp/" repo-name "/erl"))
+        (c/lit "./_rel/jtpg/bin/jtpg stop")
         (info node "building jtpg release.")
         (c/exec :make)
         (c/exec :make :release)
-        (c/lit "./_rel/jtpg/bin/jtpg start")))
+        (c/lit "/tmp/jtpg/erl/_rel/jtpg/bin/jtpg start")
+        (Thread/sleep 5000)))
 
     ;; TODO: Stop the Erlang node.
     (teardown! [_ _ _])))
@@ -32,6 +35,7 @@
 (defn create-new-pid [client n]
   (let [uri (str (.endpoint client) n)
         resp @(http/get uri)]
+    (info "Response status: " (:status resp) "response: " resp)
     (= (:status resp) 200)))
 
 (defrecord Pg2NodeListClient [endpoint]
@@ -41,9 +45,11 @@
       (Pg2NodeListClient. endpoint)))
   
   (invoke! [this test {:keys [value op] :as cmd}]
-      (if (create-new-pid this value)
-        (assoc cmd :type :ok)
-        (assoc cmd :type :fail)))
+      (let [resp (create-new-pid this value)]
+        (info "Response from creating new pid:" resp)
+        (if resp
+          (assoc cmd :type :ok)
+          (assoc cmd :type :fail))))
 
   (teardown! [_ _]
     (throw (UnsupportedOperationException.))))
