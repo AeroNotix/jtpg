27e18242980d8578592559214546c239931ba4db
Aaron France <aaron.l.france@gmail.com>
2014-05-19 16:08:15 +0200

    Derped the herp with the timing.

--- erl/src/jtpg_app.erl
+++ erl/src/jtpg_app.erl
@@ -12,13 +12,23 @@
 %% Application callbacks
 %% ===================================================================
 
+connect_to_all_nodes([]) -> ok;
+connect_to_all_nodes([Node|Nodes] = All) ->
+    case net_kernel:connect(Node) of
+        true ->
+            timer:sleep(1000),
+            connect_to_all_nodes(Nodes);
+        false ->
+            connect_to_all_nodes(All)
+    end.
+
 start(_StartType, _StartArgs) ->
-    [true, true, true, true]
-        = [net_kernel:connect(N) || N <- ['jtpg@192.168.122.11',
-                                          'jtpg@192.168.122.12',
-                                          'jtpg@192.168.122.13',
-                                          'jtpg@192.168.122.14',
-                                          'jtpg@192.168.122.15'] -- [node()]],
+    AllNodes = ['jtpg@192.168.122.11',
+                'jtpg@192.168.122.12',
+                'jtpg@192.168.122.13',
+                'jtpg@192.168.122.14',
+                'jtpg@192.168.122.15'],
+    ok = connect_to_all_nodes(AllNodes -- [node()]),
     {ok, _} = cpg:start_link(?SCOPE),
     Routes = [{<<"/new_pid/:id">>, jtpg_http_handler, []}],
     Dispatch = cowboy_router:compile([{'_', Routes}]),
