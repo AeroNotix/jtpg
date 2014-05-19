a22ced2487de6c1be7a849d34ccef29156fdcc92
Aaron France <aaron.l.france@gmail.com>
2014-05-19 23:54:08 +0200

    Use pmap for the initial connections to nodes.

--- erl/src/jtpg_util.erl
+++ erl/src/jtpg_util.erl
@@ -5,13 +5,17 @@
 
 %% This is pretty dumb since it doesn't deal with other nodes being
 %% down but hey if they aren't up then the tests fail anyway. #yolo
-connect_all_nodes([]) -> ok;
-connect_all_nodes([Node|Nodes] = All) ->
+connect_node(Node) ->
     case net_kernel:connect(Node) of
         true ->
-            connect_all_nodes(Nodes);
+            ok;
         false ->
             timer:sleep(1000),
-            connect_all_nodes(All)
+            connect_node(Node)
     end.
 
+connect_all_nodes([]) -> ok;
+connect_all_nodes(Nodes) when is_list(Nodes) ->
+    Replies = lists:duplicate(length(Nodes), true),
+    Replies = pmap:map(fun connect_node/1, Nodes),
+    ok.
